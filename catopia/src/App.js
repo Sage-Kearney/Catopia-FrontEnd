de61323baa5052376209031fcc390e53adeefed3
jnlsings <jnlsings@gmail.com>
2020-02-12 20:03:00 -0600

    Add router to index.js

--- catopia/src/App.js
+++ catopia/src/App.js
@@ -1,31 +1,33 @@
 import React, { Component } from 'react';
-import { Link, Switch, Route } from "react-router-dom";
+import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Show from './components/Show';
 import New from './components/New';
 import Edit from './components/Edit';
 
-class App extends Component () {
+class App extends Component() {
   constructor() {
     super();
     this.state = {
       cats: []
-    }
+    };
   }
 
-  componentDidMount() {
-    const catsUrl = 'https://localhost:3000/cats'
+  // componentDidMount() {
+  //   const catsUrl = 'https://localhost:3000/cats';
 
-    fetch(catUrl)
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-      this.setState({ cats: response })
-    });
+  //   fetch(catsUrl)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log(response);
+  //       this.setState({ cats: response });
+  //     });
+  // }
+
+  render() {
+    console.log(this.state.cats);
   }
-
-  render();
   return;
 }
 
