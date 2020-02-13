6d99e4eeb50205dbbadf20e979a51f42b14d6bd0
Sage-Kearney <56275819+Sage-Kearney@users.noreply.github.com>
2020-02-13 16:07:17 -0500

    worked on new component

--- catopia/src/components/New.js
+++ catopia/src/components/New.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, withRouter } from 'react-router-dom';
 
 class New extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      //   id: null,
-      //   name: '',
-      //   type: '',
-      //   personality: ''
-      //   //   cat: {},
-      //   //   cats: []
+      cat: {
+      _id:'',
+      name: '',
+      type: '',
+      img: '',
+      personality:''
+      },
       isNew: false
     };
     // this.handleChange = this.handleChange.bind(this);
@@ -19,16 +20,42 @@ class New extends Component {
     // console.log(this.addCat);
   }
 
-  handleAdd() {
+  handleChange = (evt) => {
+    const updatedField = {
+      [evt.target.id]: evt.target.value
+    }
+    const newCat = Object.assign(this.state.cat, updatedField)
+    this.setState({ cat: newCat })
+  }
+
+  handleAdd(evt) {
+    evt.preventDefault();
     const addedCatUrl = `http://localhost:4000/cats`;
+    let newCat = {
+      name: this.state.name,
+      type: this.state.type,
+      img
+    };
 
     fetch(addedCatUrl, {
-      method: 'POST'
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json;charset=UTF-8'
+      },
+      body: JSON.stringify({
+        // [evt.target.id]: evt.target.value,
+        // [evt.target.id]: evt.target.value,
+        // [evt.target.id]: evt.target.value,
+        // [evt.target.id]: evt.target.value
+      })
     })
       .then(
         this.props.history.push({
           pathname: '/',
-          state: { added: this.props.selectedCat._id }
+          state:
+            ({ added: this.props.selectedCat._id },
+            { added: this.props.selectedCat.name })
         })
       )
       .then(function refreshPage() {
@@ -39,21 +66,19 @@ class New extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={this.props.handleSubmit}>
-          Name:<input onChange={this.props.handleChange} type="text"></input>
+        <form onSubmit={this.handleAdd}>
+          Name:<input onChange={this.props.handleChange} type="text" id="name" ></input>
           Type:
-          <input onChange={this.props.handleChange} type="text"></input>
+          <input onChange={this.props.handleChange} type="text" id="type"></input>
           Img:
-          <input onChange={this.props.handleChange} type="text"></input>
+          <input onChange={this.props.handleChange} type="text" id="img"></input>
           Personality:
-          <input onChange={this.props.handleChange} type="text"></input>
-          <button type="submit" onClick={() => this.handleAdd()}>
-            Add Cat
-          </button>
+          <input onChange={this.props.handleChange} type="text" id="personality"></input>
+          <button type="submit">Add Cat</button>
         </form>
       </div>
     );
   }
 }
 
-export default New;
+export default withRouter(New);
