e1d46926f8aaba56b91d07e8a38e4ccd5f038148
jnlsings <jnlsings@gmail.com>
2020-02-12 21:32:54 -0600

    hard code data, passed as props to Home
    1. hard code data
    2. pass props to Home
    3. Display cat img and name
    4. Link to see more (show page)

--- catopia/src/App.js
+++ catopia/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -6,24 +7,57 @@ import Show from './components/Show';
 import New from './components/New';
 import Edit from './components/Edit';
 
+let cats = [
+  {
+    name: 'Gabby',
+    type: 'Domestic shorthair- black kitty',
+    img: 'https://i.imgur.com/POh2l9a.jpg',
+    personality: 'Super sweet, with a wild side!'
+  },
+  {
+    name: 'Chewy',
+    type: 'Tabby',
+    img: 'https://i.imgur.com/wQyAbf1.jpg',
+    personality: 'Lovable shithead.'
+  },
+  {
+    name: 'Simba',
+    type: 'Ocicat',
+    img: 'https://i.imgur.com/tbg9Pqz.jpg',
+    personality: 'Fiesty! Very independent'
+  },
+  {
+    name: 'Luna',
+    type: 'Burmese',
+    img: 'https://i.imgur.com/mvoACHa.jpg',
+    personality: 'She loves the moon.'
+  },
+  {
+    name: 'Flashlight',
+    type: 'Tuxedo',
+    img: 'https://i.imgur.com/W6QRvBY.jpeg',
+    personality: 'Low key, mysterious, but also loves the lime light.'
+  }
+];
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cats: []
+      cats: cats
     };
   }
 
-  componentDidMount() {
-    const catsUrl = 'https://localhost:3000/cats';
+  // componentDidMount() {
+  //   const catsUrl = 'https://localhost:4000/cats';
 
-    fetch(catsUrl)
-      .then(response => response.json())
-      .then(response => {
-        console.log(response);
-        this.setState({ cats: [response] });
-      });
-  }
+  //   axios
+  //     .get(catsUrl)
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       console.log(response);
+  //       this.setState({ cats: response });
+  //     });
+  // }
 
   render() {
     console.log(this.state.cats);
--- catopia/src/components/Home.js
+++ catopia/src/components/Home.js
@@ -3,8 +3,20 @@ import '../App.css';
 import { Link } from 'react-router-dom';
 
 function Home(props) {
-  console.log(this.props.cats);
-  return <div>props.cats.map(cat => {})</div>;
+  console.log(props.cats);
+  return (
+    <div>
+      {props.cats.map(cat => {
+        return (
+          <div>
+            <img src={cat.img}></img>
+            <h1>{cat.name}</h1>
+            <Link to={`/${cat.name}`}>See More</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Home;
