d742266cca9be6f03264cb9753e9ec992df07cc1
jnlsings <jnlsings@gmail.com>
2020-02-13 09:34:03 -0600

    Fetch api success, delete hard-coded data

--- catopia/src/App.js
+++ catopia/src/App.js
@@ -1,4 +1,3 @@
-// import axios from 'axios';
 import React, { Component } from 'react';
 import { Link, Switch, Route } from 'react-router-dom';
 import './App.css';
@@ -7,43 +6,11 @@ import Show from './components/Show';
 import New from './components/New';
 import Edit from './components/Edit';
 
-let cats = [
-  {
-    name: 'Gabby',
-    type: 'Domestic shorthair- black kitty',
-    img: 'https://i.imgur.com/POh2l9a.jpg',
-    personality: 'Super sweet, with a wild side!'
-  },
-  {
-    name: 'Chewy',
-    type: 'Tabby',
-    img: 'https://i.imgur.com/wQyAbf1.jpg',
-    personality: 'Lovable shithead.'
-  },
-  {
-    name: 'Simba',
-    type: 'Ocicat',
-    img: 'https://i.imgur.com/tbg9Pqz.jpg',
-    personality: 'Fiesty! Very independent'
-  },
-  {
-    name: 'Luna',
-    type: 'Burmese',
-    img: 'https://i.imgur.com/mvoACHa.jpg',
-    personality: 'She loves the moon.'
-  },
-  {
-    name: 'Flashlight',
-    type: 'Tuxedo',
-    img: 'https://i.imgur.com/W6QRvBY.jpeg',
-    personality: 'Low key, mysterious, but also loves the lime light.'
-  }
-];
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cats: cats,
+      cats: [],
       selectedCat: ''
     };
   }
@@ -66,17 +33,17 @@ class App extends Component {
     //   cats: prevState.cats.filter(cat => cat.id !== id)
     // }))
   };
-  // componentDidMount() {
-  //   const catsUrl = 'https://localhost:4000/cats';
 
-  //   axios
-  //     .get(catsUrl)
-  //     .then(response => response.json())
-  //     .then(response => {
-  //       console.log(response);
-  //       this.setState({ cats: response });
-  //     });
-  // }
+  componentDidMount() {
+    const catsUrl = 'http://localhost:4000/cats';
+
+    fetch(catsUrl)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.setState({ cats: response });
+      });
+  }
 
   render() {
     console.log(this.state.cats);
